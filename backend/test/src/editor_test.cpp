f7f545c060430977201e71aacef10a1ec335058d
snowz <marcel.patzwahl@gmail.com>
2015-11-25 14:36:23 +0100

    Revert "build changed to qmake"
    This reverts commit f5f63b0fa57c3c945a67fc4586b2389feab25005.

---
+++ backend/test/src/editor_test.cpp
@@ -0,0 +1,26 @@
+#define BOOST_TEST_MAIN
+#include <boost/test/included/unit_test.hpp>
+
+#include "editor.hpp"
+#include "window.hpp"
+
+BOOST_AUTO_TEST_SUITE(editor)
+
+//Fixture needed for every test
+yace::Editor* editor = yace::Editor::getInstance();
+
+BOOST_AUTO_TEST_CASE(singleton) {
+    yace::Editor* editor2 = yace::Editor::getInstance();
+    BOOST_CHECK_EQUAL(editor, editor2);
+}
+
+BOOST_AUTO_TEST_CASE(one_window_on_creation) {
+    BOOST_CHECK(editor->getWindow(0) != nullptr);
+}
+
+BOOST_AUTO_TEST_CASE(add_a_new_window) {
+    editor->newWindow();
+    BOOST_CHECK(editor->getWindow(1) != nullptr);
+}
+
+BOOST_AUTO_TEST_SUITE_END()
