fefa402a9d39f9e9b6f4cc56de54bede53132c57
snowz <marcel.patzwahl@gmail.com>
2015-11-28 13:48:51 +0100

    second try

---
+++ backend/src/editor_py.cpp
@@ -0,0 +1,10 @@
+#include <boost/python.hpp>
+
+#include "editor.hpp"
+
+namespace yace {
+/*    BOOST_PYTHON_MODULE(yace_py) {*/
+        //boost::python::class_<Editor>("Editor", boost::python::no_init)
+              //.def("getInstance", &Editor::getInstance, boost::python::return_value_policy<boost::python::reference_existing_object>()).staticmethod("getInstance");
+    /*}*/
+}
